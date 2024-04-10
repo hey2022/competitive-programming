9a958236a597065a8cf91fe79fea710543e2e346
Yiheng He <yiheng.he@proton.me>
2024-04-11 02:39:51 +0800

    feat(kattis): basketballoneonone

---
+++ kattis/basketballoneonone.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    cout << s[s.length() - 2];
+}
